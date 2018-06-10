adb2ef0af0c46950ac6d4ddb4453fe4686c74659
Åke Forslund <ake.forslund@gmail.com>
2018-06-10 13:36:39 +0200

    Add basic tests

---
+++ test/__init__.py
@@ -0,0 +1,20 @@
+from unittest.mock import MagicMock, patch
+from test.integrationtests.skills.skill_tester import SkillTest
+
+def test_runner(skill, example, emitter, loader):
+
+    # Get the skill object from the skill path
+    s = [s for s in loader.skills if s and s.root_dir == skill]
+    class MockSettings(dict):
+        def store(self):
+            pass
+        def stop_polling(self):
+            pass
+
+    settings = MockSettings()
+    settings['enabled'] = 'true'
+    s[0].settings = settings
+    with patch(s[0].__module__ + '.remove_file') as mock_remove:
+        ret = SkillTest(skill, example, emitter).run(loader)
+    settings['enabled'] = 'false'
+    return ret
